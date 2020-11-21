3ff5eb7f17b81ce32e0a575aba8fc8edce8db698
Micke Berg <micke.berg.dev@gmail.com>
2020-11-21 17:49:00 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToDoItemComponent } from './components/to-do-item/to-do-item.component';
 
 @NgModule({
-  declarations: [AppComponent, TodoListComponent],
+  declarations: [AppComponent, TodoListComponent, ToDoItemComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ src/app/components/to-do-item/to-do-item.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'todo-item',
+  templateUrl: './to-do-item.component.html',
+  styleUrls: ['./to-do-item.component.scss'],
+})
+export class ToDoItemComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
