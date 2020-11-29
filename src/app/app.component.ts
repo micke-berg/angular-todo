50b4eb529ade411a2b52bcaff518d5af00236c8a
Micke Berg <micke.berg.dev@gmail.com>
2020-11-29 19:14:09 +0100

    Import TodoService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { TodoItem } from './Models/todo';
+import { TodoService } from './services/todo.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  providers: [TodoService],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular-Todo-App';
+
+  constructor(private todoService: TodoService) {}
+
+  ngOnInit(): void {}
 }
